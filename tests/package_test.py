c3ea649fa351f0bd5b74f6485355997d80f129b7
jclian91 <1137061634@qq.com>
2024-01-23 10:30:02 +0800

    feat: add test code

--- tests/package_test.py
+++ tests/package_test.py
@@ -6,3 +6,6 @@ from token_counter.token_count import TokenCounter
 
 text = "who are you?"
 print(TokenCounter().count(_input=text))
+
+texts = ["who are you?", "Python is fun!", "苹果手机"]
+print(TokenCounter().count(_input=texts))
